21d75edd2321a665c3faaef7853a3f992fd1f3b7
Francesca Primavera <primavera@fedepd.novalocal>
2025-10-30 13:26:08 +0100

    preselection code

---
+++ samples.h
@@ -0,0 +1,6 @@
+std::vector<std::string> samples_ssWWTT = {
+  "/lustre/cmsdata/tosi/Hmumu/mc/2024/VBF-Hto2Mu_Par-M-125_TuneCP5_13p6TeV_amcatnlo-pythia8_150X_mcRun3_2024_realistic_v2_nanoAODv15/a070b227-6eee-4174-8e5c-7af4ba14007f.root"
+  };
+
+std::vector<std::string> samples_ssWWLL = {"/lustre/cmsdata/tosi/Hmumu/mc/2024/VBF-Hto2Mu_Par-M-125_TuneCP5_13p6TeV_amcatnlo-pythia8_150X_mcRun3_2024_realistic_v2_nanoAODv15/a070b227-6eee-4174-8e5c-7af4ba14007f.root"
+};
